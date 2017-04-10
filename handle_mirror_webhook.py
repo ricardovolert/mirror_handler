708cf1177992193d7d84aa00f7072b8e72718fdf
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-04-10 13:29:39 -0500

    Add basic handler for syncing

--- handle_mirror_webhook.py
+++ handle_mirror_webhook.py
@@ -1,11 +1,22 @@
-from flask import Flask, request
-import json
+from flask import Flask
 
 app = Flask(__name__)
 app.config['DEBUG'] = False
 
+REPO_PATH = './yt-hg'
+BB_REPO = 'https://bitbucket.org/ngoldbaum/yt'
+GH_REPO = 'git+ssh://git@github.com:ngoldbaum/yt-mirror.git'
+
 @app.route('/',methods=['POST'])
 def foo():
-    data = json.loads(request.data)
-    print(data)
+    try:
+        repo = hglib.open(REPO_PATH)
+        repo.close()
+    except hglib.error.ServerError:
+        hglib.clone(source=BB_REPO, dest=REPO_PATH)
+
+    with hglib.open(REPO_PATH) as repo:
+        repo.pull(BB_REPO)
+        repo.push(GH_REPO)
+
     return "OK"
