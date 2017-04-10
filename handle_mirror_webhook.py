2c4fe84a14ce0d8a1f2a025f43185fee5c6f2d11
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2017-04-10 16:09:14 -0500

    Make a local clone of github repo
    This allows to circumvent the hggit's inability to push to repo
    using https

--- handle_mirror_webhook.py
+++ handle_mirror_webhook.py
@@ -1,32 +1,45 @@
+import git
 import hglib
 import os
 from flask import Flask
 
+
+GH_REPO_PATH = os.environ.get('GH_REPO_PATH', '/tmp/yt-git')
+BB_REPO_PATH = os.environ.get('BB_REPO_PATH', '/tmp/yt-hg')
+BB_REPO = os.environ.get('BB_REPO', 'https://bitbucket.org/ngoldbaum/yt')
+GH_REPO = os.environ.get(
+    'GH_REPO', 'git+ssh://git@github.com:ngoldbaum/yt-mirror.git')
+
 app = Flask(__name__)
 app.config['DEBUG'] = False
 
-def get_environment_variable(variable, default):
-    try:
-        return os.environ[variable]
-    except KeyError:
-        return default
-
-REPO_PATH = get_environment_variable('REPO_PATH', './yt-hg')
-BB_REPO = get_environment_variable('BB_REPO', 'https://bitbucket.org/ngoldbaum/yt')
-GH_REPO = get_environment_variable(
-    'GH_REPO', 'git+ssh://git@github.com:ngoldbaum/yt-mirror.git')
 
 @app.route('/', methods=['POST'])
 def foo():
-    configs = ['extensions.hgext.bookmarks=', 'extensions.hggit=']
+    configs = ['extensions.hggit=']
     try:
-        repo = hglib.open(REPO_PATH, configs=configs)
+        repo = hglib.open(BB_REPO_PATH, configs=configs)
         repo.close()
     except hglib.error.ServerError:
-        hglib.clone(source=BB_REPO, dest=REPO_PATH)
+        hglib.clone(source=BB_REPO, dest=BB_REPO_PATH)
 
-    with hglib.open(REPO_PATH, configs=configs) as repo:
+    try:
+        gh_repo = git.Repo(GH_REPO_PATH)
+        origin = gh_repo.remote('origin')
+        origin.pull('master')
+        gh_repo.close()
+    except git.exc.NoSuchPathError:
+        gh_repo = git.Repo.init(GH_REPO_PATH)
+        origin = gh_repo.create_remote('origin', GH_REPO)
+        origin.pull('master')
+        gh_repo.close()
+
+    with hglib.open(BB_REPO_PATH, configs=configs) as repo:
         repo.pull(BB_REPO)
-        repo.push(GH_REPO)
+        repo.push(GH_REPO_PATH)
+
+    with git.Repo(GH_REPO_PATH) as repo:
+        origin = repo.remote('origin')
+        origin.push('master')
 
     return "OK"
