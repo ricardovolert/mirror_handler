388269e624e2d0496128272ac8c01b3054a6d964
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-04-10 14:30:30 -0500

    add readme

--- handle_mirror_webhook.py
+++ handle_mirror_webhook.py
@@ -1,12 +1,20 @@
 import hglib
+import os
 from flask import Flask
 
 app = Flask(__name__)
 app.config['DEBUG'] = False
 
-REPO_PATH = './yt-hg'
-BB_REPO = 'https://bitbucket.org/ngoldbaum/yt'
-GH_REPO = 'git+ssh://git@github.com:ngoldbaum/yt-mirror.git'
+def get_environment_variable(variable, default):
+    try:
+        return os.environ[variable]
+    except KeyError:
+        return default
+
+REPO_PATH = get_environment_variable('REPO_PATH', './yt-hg')
+BB_REPO = get_environment_variable('BB_REPO', 'https://bitbucket.org/ngoldbaum/yt')
+GH_REPO = get_environment_variable(
+    'GH_REPO', 'git+ssh://git@github.com:ngoldbaum/yt-mirror.git')
 
 @app.route('/',methods=['POST'])
 def foo():
